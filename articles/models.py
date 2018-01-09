821533ca0a7bb7fe5b664d251c883a0ae934ff2d
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-01-08 22:03:11 -0800

    Implement basic article model, urls, and view

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # thumb = models.ImageField(default='default.png', blank=True)
+    # author later
+
+    # string representation
+    def __str__(self):
+        return self.title
