ccb0c16c418332895e60d0ac463a590ddb3a1e01
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-01-14 12:12:45 -0800

    Rename template to be better URL friendly

--- articles/views.py
+++ articles/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    return render(request, 'articles/article-list.html')
